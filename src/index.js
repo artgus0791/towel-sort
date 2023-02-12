1e79dca8840e2f22c04b95375cb8202518d8932a
artgus0791 <105542363+artgus0791@users.noreply.github.com>
2023-02-12 23:07:51 +0300

    fix:Add a parameter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 // You should implement your task here.
-module.exports = function towelSort (matrix) {
+module.exports = function towelSort (matrix = []) {
 let result = [];
 for(let i = 0 ; i < matrix.length; i++){
   i % 2 === 0 ? result = result.concat(matrix[i]):result = result.concat(matrix[i].reverse());
