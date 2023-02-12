e8b5119b2b5cb128f2198db116f26e0f49904c97
artgus0791 <105542363+artgus0791@users.noreply.github.com>
2023-02-12 22:33:02 +0300

    fix:update variable name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 module.exports = function towelSort (matrix) {
 let result = [];
 for(let i = 0 ; i < matrix.length; i++){
-  result = result.concat(hello[i]);
+  result = result.concat(matrix[i]);
 }
   return result;
 }
