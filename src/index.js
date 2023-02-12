7b65e0fe641050d9671f927a2757a99bcfd016fc
artgus0791 <105542363+artgus0791@users.noreply.github.com>
2023-02-12 22:55:53 +0300

    fix: removed the bracket

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ let result = [];
 for(let i = 0 ; i < matrix.length; i++){
   i % 2 === 0 ? result = result.concat(matrix[i]):result = result.concat(matrix[i].reverse());
   }
-}
   return result;
 }
